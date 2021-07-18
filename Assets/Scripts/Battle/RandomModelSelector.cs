45ba9cca269074b89b79f53f5b3ec02d5b20e345
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-07-18 13:32:30 +0300

    Implement random model selection for the enemy [closes #3]

---
+++ Assets/Scripts/Battle/RandomModelSelector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Battle
+{
+    public class RandomModelSelector : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _options = default;
+
+        private void Awake()
+        {
+            var selectedIndex = Random.Range(0, _options.Length);
+
+            for (var index = 0; index < _options.Length; index++)
+            {
+                _options[index].SetActive(selectedIndex == index);
+            }
+        }
+    }
+}
